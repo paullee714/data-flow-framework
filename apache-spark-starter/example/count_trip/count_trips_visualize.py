3c0d67015e336dd9e4af35a9be90aadf88e8849e
paullee714 <paul_lee@kakao.com>
2022-06-17 13:04:04 +0900

    fix: 폴더 위치 변경

--- apache-spark-starter/example/count_trip/count_trips_visualize.py
+++ apache-spark-starter/example/count_trip/count_trips_visualize.py
@@ -2,6 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-trips = pd.read_csv("../data/result_data/result_trips_data.csv")
+trips = pd.read_csv("../../data/result_data/result_trips_data.csv")
 trips.plot()
 plt.show()
